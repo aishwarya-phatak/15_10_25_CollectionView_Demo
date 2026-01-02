301fee571742f672aa17dfa36c519bcfce1d6ffe
aishwarya <ashp17star@gmail.com>
2026-01-02 13:17:54 +0530

    collection view with navigation

---
+++ 15_10_25_CollectionView_Demo/StudentDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudentDetailsViewController.swift
+//  15_10_25_CollectionView_Demo
+//
+//  Created by Vishal Jagtap on 02/01/26.
+//
+
+import UIKit
+
+class StudentDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
